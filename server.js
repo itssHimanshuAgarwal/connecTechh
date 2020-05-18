83a6671bc0e96540e6713d61c81036a1c7b68ff4
HIMANSHU R AGARWAL <himanshu59cool@gmail.com>
2020-05-18 22:24:08 +0530

    done

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
-//init Middleware
-app.use(express.json({ extended: false }));
+
+// Init Middleware
+app.use(express.json());
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -14,10 +16,11 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-//Serve static assets in production
-if (process.env.NODE_ENV === 'poduction') {
-  //Set static folder
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
@@ -25,4 +28,4 @@ if (process.env.NODE_ENV === 'poduction') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
